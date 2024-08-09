2a1306b31ca76f66b5bbc1864bd44c6b4942bab9
ChongYuHau <ChongYu.Hau@edu.sait.ca>
2024-08-08 21:59:58 -0600

    styling_homepage

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import Link from 'next/link';
- 
+
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-700 mb-8">Welcome to the Weather App</h1>
-      <p className="text-lg text-gray-600 mb-4">Get weather information by entering your postal code.</p>
-      <Link href="/components"> Enter Postal Code</Link>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-sky-200 text-black">
+      <h1 className="text-6xl font-extrabold text-center mb-8">Welcome to the Weather App</h1>
+      <p className="text-lg mb-6 text-center">Get weather information by entering your postal code.</p>
+      <Link href="/components">
+        <button className="bg-blue-600 text-white font-bold py-3 px-6 rounded-lg shadow-lg hover:bg-blue-700 transition duration-300 focus:outline-none focus:ring-2 focus:ring-blue-300">
+          Enter Postal Code
+        </button>
+      </Link>
     </div>
   );
 }
